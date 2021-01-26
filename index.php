6e1569d63a66c2d648980e9543fc295026590ef8
James Davis <davisjam3s@gmail.com>
2021-01-26 19:42:11 +0000

    Code Clean UP

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-$serverName = "localhost";
-$username = "root";
-$password = "";
-$dbname = "shift_site_test";
-$conn = mysqli_connect($serverName, $username, $password, $dbname);
-if (!$conn)
-{
-	die("Connection failed: " . mysqli_connect_error()); // check for connection error
-}
-?>
+<?php include'php/Connection.php'; ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -49,153 +39,18 @@ if (!$conn)
 				</div>
 				
 			</div>
+			
 			<div class="flex_Container_Item">
-				<?php
-				$date_Current = date('Y/m/d');
-				$Date_Last_Sunday = date('Y/m/d', strtotime('last Sunday', strtotime($date_Current)));
-				$Date_Next_Saturday = date('Y/m/d', strtotime('next saturday', strtotime($date_Current)))  ;
-				echo "Todays date is: $date_Current <br> Week Begining: $Date_Last_Sunday <br> Week Ends: $Date_Next_Saturday ";
-				?>
+				<?php include'php/SunToSatDates.php'; ?>
 			</div>
 			
 		</div>
         <label class="Clock_In_Message"></label>
 		<br>
 		
-		<?php
-			$sql = "SELECT * FROM users INNER JOIN stores ON users.Store_ID = stores.Store_ID;";
-	
-			$result = $result = mysqli_query($conn, $sql);
-			if (mysqli_num_rows($result) > 0)
-			{
-									echo"
-					<table>
-					<tr class='Titles'>
-						<th>User ID</th>
-						<th>First name</th>
-						<th>Surname</th>
-						<th>Store</th>
-						<th>Admin Status</th>
-					</tr>
-					";
-				while ($row = mysqli_fetch_assoc($result))
-				{
-					$UserID = $row["User_ID"];
-					$UserFirstname = $row["Firstname"];
-					$UserSurname = $row["Surname"];
-					//$UserStoreID = $row["Store_ID"];
-					$CheckAdmin = $row["IsAdmin"];
-					$StoreName = $row["Store_Name"];
-					
-					if($CheckAdmin == 0)
-					{$CheckAdmin = "No";}else{$CheckAdmin = "Yes";}
-					
-
-					echo"
-					<tr class=''>
-					<td>$UserID</td>
-					<td>$UserFirstname</td>
-					<td>$UserSurname</td>
-					<td>$StoreName</td>
-					<td>$CheckAdmin</td>
-					</tr>
-					
-					"; 
-
-					
-				}
-				echo"</table>";
-			}else{echo "0 results";}
+		<?php include'php/ShowUsers.php';?>
 		
-		?>
-		
-		<?php
-			$sql_shifts = "SELECT * FROM shifts
-INNER JOIN users ON shifts.User_ID=users.User_ID
-INNER JOIN stores ON shifts.Store_ID=stores.Store_ID
-INNER JOIN pay_codes ON shifts.Pay_Code_ID=pay_codes.Pay_Code_ID
-WHERE Start_Time_Date BETWEEN '$Date_Last_Sunday' AND '$Date_Next_Saturday'";
-		
-			$result_Shifts = $result_Shifts = mysqli_query($conn, $sql_shifts);
-			if (mysqli_num_rows($result) > 0)
-			{
-			echo"
-					<table>
-					<tr class='Titles'>
-						<th>Shift ID</th>
-						<th>Name</th>
-						<th>Store</th>
-						<th>Start Date</th>
-						<th>End Date</th>
-						<th>Start Time</th>
-						<th>Clock In Time</th>
-						<th>End Time</th>
-						<th>Clock Out Time</th>
-						<th>Pay Code</th>
-						<th>Shift Length</th>
-						<th>Break Length</th>
-					</tr>
-					";
-				while ($row_Shifts = mysqli_fetch_assoc($result_Shifts))
-				{
-					$Shift_ID 				= $row_Shifts["Shift_ID"];
-					$Shift_Firsname 		=$row_Shifts["Firstname"];
-					$Shift_Surname 			=$row_Shifts["Surname"];
-					$Shift_Store_Name 		=$row_Shifts["Store_Name"];
-					$Shift_Start_Day 		=$row_Shifts["Start_Time_Date"];
-					$Shift_End_Day 			=$row_Shifts["Shift_End_Date"];
-					$Shift_Start_Time 		=$row_Shifts["Start_Time"];
-					$Shift_ClockIn_Time 	=$row_Shifts["Clocked_In_Time"];
-					$Shift_End_Time 		=$row_Shifts["End_Time"];
-					$Shift_ClockOut_Time 	=$row_Shifts["Clocked_Out_Time"];
-					$Shift_Paycode 			=$row_Shifts["Pay_Code_Type"];
-					
-					$Time1 = strtotime($Shift_Start_Time);
-					$Time2 = strtotime($Shift_End_Time);
-					$Shift_Length = round(abs($Time2 - $Time1) / 3600,2);
-					$Break_Length = "";
-					if($Shift_Length <= 4.5){
-						$Break_Length = "No Break";
-					}
-					if($Shift_Length >=4.5 && $Shift_Length <6)
-					{
-						$Break_Length = "15 Mins";
-					}
-					if($Shift_Length >=6 && $Shift_Length <8)
-					{
-						$Break_Length = "20 Mins";
-					}
-					if($Shift_Length >=8 && $Shift_Length <9)
-					{
-						$Break_Length = "30 Mins";
-					}
-					if($Shift_Length > 9)
-					{
-						$Break_Length = "45 Mins";
-					}
-					
-					echo"
-					<tr class=''>
-					<td>$Shift_ID</td>
-					<td>$Shift_Firsname $Shift_Surname</td>
-					<td>$Shift_Store_Name</td>
-					<td>$Shift_Start_Day</td>
-					<td>$Shift_End_Day </td>
-					<td>$Shift_Start_Time</td>
-					<td>$Shift_ClockIn_Time</td>
-					<td>$Shift_End_Time</td>
-					<td>$Shift_ClockOut_Time</td>
-					<td>$Shift_Paycode</td>
-					<td>$Shift_Length Hours</td>
-					<td>$Break_Length</td>
-					";
-				}
-				echo "</table";
-				
-			}else{
-				echo "No results";
-			}
-		?>
+		<?php include'php/CheckShifts.php';?>
 
 		
     </body>
